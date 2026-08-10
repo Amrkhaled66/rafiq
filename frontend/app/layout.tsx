7ada00008590b3fd1144653d6c3598472422248f
Amrkhaled66 <amrse912@gmail.com>
2026-08-10 06:22:12 +0300

    update pixel

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -53,6 +53,7 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '${pixelId}');
+                  fbq('track', 'PageView');
               `}
             </Script>
             <noscript>
