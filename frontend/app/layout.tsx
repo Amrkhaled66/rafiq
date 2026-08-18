f1d07c590ba6113cdb967ac278933400213c2ebf
Amrkhaled66 <amrse912@gmail.com>
2026-08-19 02:41:05 +0300

    feat:add google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -111,27 +111,44 @@ export default function RootLayout({
         {children}
 
         <Footer />
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KL61XXR9W8"
+          strategy="afterInteractive"
+        />
 
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KL61XXR9W8');
+    `}
+        </Script>
+
+        {/* Meta Pixel */}
         {pixelId && (
           <>
             <Script id="meta-pixel" strategy="afterInteractive">
               {`
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(
-                  window,
-                  document,
-                  'script',
-                  'https://connect.facebook.net/en_US/fbevents.js'
-                );
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(
+            window,
+            document,
+            'script',
+            'https://connect.facebook.net/en_US/fbevents.js'
+          );
 
-                fbq('init', '${pixelId}');
-                fbq('track', 'PageView');
-              `}
+          fbq('init', '${pixelId}');
+          fbq('track', 'PageView');
+        `}
             </Script>
 
             <noscript>
